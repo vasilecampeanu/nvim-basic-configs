36bdeb2b8d700691e964f40338637840dbb67ebf
vasilecampeanu <vasilecampeanu.link@yahoo.com>
2022-07-11 16:07:06 +0300

    feat: Add core configurations.

---
+++ lua/user/core/init.lua
@@ -0,0 +1,5 @@
+-- Default options
+require("user.core.colorscheme")
+require("user.core.options")
+require("user.core.custom-vim-style")
+require("user.core.keymaps")
--- lua/user/custom.lua
+++
@@ -1,5 +0,0 @@
--- Remove vim background colore and use the terminal color.
-vim.cmd [[hi Normal guibg=NONE ctermbg=NONE]]
-
--- Remove sign bar background colore.
-vim.cmd [[highlight SignColumn guibg=NONE]]
